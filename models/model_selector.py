a16882ec10b3dc4fe2343125256902c75a1ec429
Bruno Jerkovic <brunojerkovi@gmail.com>
2023-01-14 23:15:04 +0100

    server_commit

--- models/model_selector.py
+++ models/model_selector.py
@@ -2,6 +2,7 @@ from models.model import Model
 from models.nri.main import NRI
 from models.ngc.main import NGC
 from models.tvar.main import TVAR
+from models.ngc_noise.main import NGC as NGCSparse
 
 def model_selector(config, result_saver) -> Model:
     MyModel = None
@@ -17,5 +18,8 @@ def model_selector(config, result_saver) -> Model:
     elif config.model == 'tvar' or config.model == 4:
         MyModel = TVAR
         config.update(config.tvar)
+    elif config.mdoel == 'ngc_sparse' or config.model == 5:
+        MyModel = NGCSparse
+        config.update(config.ngc_noise)
 
     return MyModel(config, result_saver)
